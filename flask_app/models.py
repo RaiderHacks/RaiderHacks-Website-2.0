40d258095dcdaab36ef2f150e5e86dd4a160e035
Pranjal Ghimire <ghimirepranjal@protonmail.com>
2020-12-16 14:39:55 -0600

    finished addachievements table

--- flask_app/models.py
+++ flask_app/models.py
@@ -28,3 +28,13 @@ class Post(db.Model):
 
     def __repr__(self):
         return(self.id)
+
+class Addachievements(db.model):
+    achievementID= db.Column(db.Integer, primary_key=True)
+    postTitle= db.Column(db.String(120), nullable=False)
+    postImage= db.Column(db.LargeBinary, nullable=False)
+    content= db.Column(db.String(120), nullable=False)
+    userID= db.Column(db.Integer,db.ForeignKey('user.id'), nullable=False)
+
+    def __repr__(self):
+        return(self.id)
