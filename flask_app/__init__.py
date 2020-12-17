7a299e9be78b9adf63973130979e42f0832ec84d
notjoemartinez <notjoemartinez@protonmail.com>
2020-12-16 19:24:52 -0600

    working email auth system

--- flask_app/__init__.py
+++ flask_app/__init__.py
@@ -1,13 +1,23 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
-from flask_admin import Admin
+from flask_mail import Mail, Message
 
 
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = '1A37BbcCJh67'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+
+app.config['MAIL_SERVER']='smtp.gmail.com'
+app.config['MAIL_PORT'] = 465
+app.config['MAIL_USERNAME'] = 'raiderHacksMail@gmail.com'
+app.config['MAIL_PASSWORD'] = ''
+app.config['MAIL_USE_TLS'] = False
+app.config['MAIL_USE_SSL'] = True
+
+mail = Mail(app)
+
 db = SQLAlchemy(app)
 
 
--- flask_app/models.py
+++ flask_app/models.py
@@ -6,6 +6,10 @@ from flask_login import UserMixin
 def load_user(user_id):
     return User.query.get(int(user_id))
 
+# 1 user submitted application
+# 2 user manually acceptaed 
+# 3 user is an admin 
+
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     # username = db.Column(db.String(20), unique=True, nullable=False)
@@ -13,11 +17,11 @@ class User(db.Model, UserMixin):
     last_name = db.Column(db.String(40))
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(60), nullable=False)
-    admin = db.Column(db.Boolean(), nullable=False)
+    permissions = db.Column(db.Integer(), nullable=False, default=1)
     posts = db.relationship('Post', backref='author', lazy=True)
 
     def __repr__(self):
-        return(self.email)
+        return(self.id)
 
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
