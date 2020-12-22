1112718b34e6fb56b396c144d2e71ef69c1b055a
derby <derby@derbys-MBP.lan>
2020-12-22 17:49:26 -0600

    Added anon, loggedin and authenticated user roles

--- flask_app/forms.py
+++ flask_app/forms.py
@@ -1,8 +1,9 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, TextAreaField
+from wtforms import StringField, SubmitField, TextAreaField, RadioField
 from wtforms.validators import DataRequired
 
 class PostForm(FlaskForm):
     title = StringField('Title', validators=[DataRequired()])
     content = TextAreaField('Content', validators=[DataRequired()])
+    permissions = RadioField('Permissions:', choices=[(1,'Level 1: Everyone'),(2,'Level 2: Authenticated')], validators=[DataRequired()])
     submit = SubmitField('Post')
