dd3773a686b4a0ba802ffe5f4d6becd71dfccf6b
notjoemartinez <notjoemartinez@protonmail.com>
2020-12-28 21:40:52 -0600

    markdown

--- raider_hacks/__init__.py
+++ raider_hacks/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_mail import Mail, Message
+from flaskext.markdown import Markdown
 
 
 app = Flask(__name__)
@@ -19,8 +20,8 @@ app.config['MAIL_USE_SSL'] = True
 mail = Mail(app)
 
 db = SQLAlchemy(app)
-
-
+# initalize markdown
+md = Markdown(app)
 ## login manager stuff
 login_manager = LoginManager()
 login_manager.init_app(app) # this inti_app
